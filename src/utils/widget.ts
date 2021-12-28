5e0f46927dd87b94aeedd25661b2090a69067e28
Ourai Lin <ourairyu@gmail.com>
2021-12-28 16:50:07 +0800

    feat: render arrangeable form fields

--- src/utils/widget.ts
+++ src/utils/widget.ts
@@ -1,10 +1,11 @@
-import { isString } from '@ntks/toolbox';
+import { isString, isNumeric } from '@ntks/toolbox';
 
 import {
   ClientAction,
   ViewContext,
   ComponentRenderer,
   ComponentCtor,
+  InputDescriptor,
   ModuleContext,
   getAppHelper,
   isWidgetDependency,
@@ -69,4 +70,53 @@ function executeClientAction(
   }
 }
 
-export { resolveWidgetCtor, executeClientAction };
+function renderFormFieldNodes<
+  DescriptorType extends InputDescriptor & { hidden?: boolean },
+  NodeType
+>(
+  descriptors: DescriptorType[],
+  arrangement: string,
+  renderChild: (descriptor: DescriptorType) => NodeType,
+  renderRow: (descriptors: DescriptorType[], fakeIndex: number) => NodeType,
+): NodeType[] {
+  const formFieldNodes: NodeType[] = [];
+  const rows = (arrangement || '').split('|') as any[];
+
+  let needLayout = false;
+
+  if (rows.length > 0) {
+    const availableRows: number[] = [];
+
+    rows.forEach(row => {
+      if (isNumeric(row) && Number(row) > 0) {
+        availableRows.push(row);
+      }
+    });
+
+    needLayout = availableRows.length === rows.length;
+  }
+
+  if (needLayout) {
+    const remainedFilters = descriptors.filter(({ hidden }) => hidden !== true);
+
+    do {
+      formFieldNodes.push(
+        renderRow(remainedFilters.splice(0, rows.shift() * 1), remainedFilters.length),
+      );
+    } while (remainedFilters.length > 0 && rows.length > 0);
+
+    if (remainedFilters.length > 0) {
+      formFieldNodes.push(renderRow(remainedFilters, -1));
+    }
+  } else {
+    descriptors.forEach(descriptor => {
+      if (!descriptor.hidden) {
+        formFieldNodes.push(renderChild(descriptor));
+      }
+    });
+  }
+
+  return formFieldNodes;
+}
+
+export { resolveWidgetCtor, executeClientAction, renderFormFieldNodes };
