2f81619ba2a9463ba0d8040ed9304efb251ac314
Ourai Lin <ourairyu@gmail.com>
2022-01-09 10:22:53 +0800

    feat: component descriptor resolver

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,7 @@ export * from './string';
 export * from './is';
 export * from './input';
 export * from './theme';
+export { resolveComponentDescriptors } from './component';
 export { resolveWidgetCtor, renderFormFieldNodes } from './widget';
 export * from './renderer';
 export * from './app';
